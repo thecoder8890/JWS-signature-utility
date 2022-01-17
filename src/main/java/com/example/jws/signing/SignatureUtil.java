490625c945dabdf75fdf1488ced7ecd8e2af07b6
Navjyot Haryal <navjyotharyal@gmail.com>
2022-01-17 15:19:44 +0530

    document update

--- src/main/java/com/example/jws/signing/SignatureUtil.java
+++ src/main/java/com/example/jws/signing/SignatureUtil.java
@@ -24,13 +24,19 @@ public class SignatureUtil {
 
 	private static String[] CRIT = new String[] { "iat" };
 	private static JWSAlgorithm alg = JWSAlgorithm.RS256;
-	private static final String kID = "1SHJ5AWiI6Va1H2xiBsjOO86qNg"; 
+	private static final String kID = "dGhlIHF1aWNrIGJyb3duIGZveCBqdW1wcyBvdmVyIHRoZSBsYXp5IGRvZw";//This sample Kid 
 
 	public static void main(String arg[]) {
 		try {
 			RSAPrivateKey rasPrivateKey = loadTestPrivateKey();
 			String file = "src/main/resources/RequestPayload.json";
 			String json = readFileAsString(file);
+			// For iat
+			// Identifies the time at which the message was signed. It can be used to
+			// determine the age of the signature. Its value must be a number containing a
+			// NumericDate value (epoch timestamp generated based on UTC timestamp). Signer
+			// must always populate it. Verifier might choose to validate it to prevent
+			// replay attacks.
 			Long iat = System.currentTimeMillis() / 1000L;
 			String signedRequest = createSignature(rasPrivateKey, json, kID, iat.toString());
 			System.out.println("signedRequest " + signedRequest);
@@ -38,12 +44,12 @@ public class SignatureUtil {
 			e.printStackTrace();
 		}
 	}
-	
-	private static String readFileAsString(String file)throws Exception
-    {
-        return new String(Files.readAllBytes(Paths.get(file)));
-    }
 
+	private static String readFileAsString(String file) throws Exception {
+		return new String(Files.readAllBytes(Paths.get(file)));
+	}
+
+	// Load signing key to and convert it into PKCS8EncodedKeySpec
 	private static RSAPrivateKey loadTestPrivateKey()
 			throws NoSuchAlgorithmException, InvalidKeySpecException, IOException {
 		System.out.println("Executing-Loading_Private_Key");
@@ -51,6 +57,9 @@ public class SignatureUtil {
 		return (RSAPrivateKey) KeyFactory.getInstance("RSA")
 				.generatePrivate(new PKCS8EncodedKeySpec(PemFileContentReader.getContent(certIn)));
 	}
+
+	// Creating the signature with the key, payload, kid, iat. And returns JWS
+	// signature
 
 	private static String createSignature(RSAPrivateKey key, String payload, String kid, String iat) throws Exception {
 		System.out.println("Executing-Creating_Signature");
@@ -61,17 +70,16 @@ public class SignatureUtil {
 		map.put("crit", CRIT);
 
 		Gson gson = new GsonBuilder().disableHtmlEscaping().setPrettyPrinting().create();
-		final String header = Base64.getUrlEncoder(
-				).withoutPadding().encodeToString(gson.toJson(map).getBytes());
+		final String header = Base64.getUrlEncoder().withoutPadding().encodeToString(gson.toJson(map).getBytes());
 
 		ByteArrayOutputStream os = new ByteArrayOutputStream();
 		os.write(header.getBytes());
 		os.write(0x2e); // ascii for "."
 		os.write(payload.getBytes());
 
-		// please note that the new JWSHeader(alg) just selects the algorithm to use to sign the request with.
+		// please note that the new JWSHeader(alg) just selects the algorithm to use to
+		// sign the request with.
 		return header + ".." + new RSASSASigner(key).sign(new JWSHeader(alg), os.toByteArray());
 	}
 
 }
-
